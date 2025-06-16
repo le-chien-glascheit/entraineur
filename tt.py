2cffdf258a39cda20d07222e8909757097e64ff9
Vladislav <166797675+le-chien-glascheit@users.noreply.github.com>
2025-06-16 14:25:12 +0300

    Update tt.py

--- tt.py
+++ tt.py
@@ -1,155 +1,68 @@
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
-from sqlalchemy.orm import sessionmaker, relationship, declarative_base
-from polifactory import Factory, List, Lazy, Seed
+from polyfactory import SQLAlchemyFactory
+from sqlalchemy.orm import relationship
+from my_project import models  # импортируйте ваши модели
 
-Base = declarative_base()
 
-# Определяем модели
-class Parent(Base):
-    __tablename__ = 'parents'
+class EmployeeFactory(SQLAlchemyFactory[models.Employee]):
+    __model__ = models.Employee
+
+    # Укажите значения полей по умолчанию, при необходимости
+    username = "test_user"
+    roles = ["user"]
+
+
+class WebinarFactory(SQLAlchemyFactory[models.Webinar]):
+    __model__ = models.Webinar
+
+    # Фабрика для создания вебинаров
+    event_id = None
+    date = "2023-01-01"
+    duration = 60
+    link = "http://example.com"
     
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    children = relationship("Child", back_populates="parent")
-
-class Child(Base):
-    __tablename__ = 'children'
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    parent_id = Column(Integer, ForeignKey('parents.id'))
-    parent = relationship("Parent", back_populates="children")
-
-# Создаем фабрики
-class ParentFactory(Factory):
-    class Meta:
-        model = Parent
-
-    id = Lazy(lambda: Seed(1, 1))
-    name = List(["Parent 1", "Parent 2", "Parent 3"])
-
-class ChildFactory(Factory):
-    class Meta:
-        model = Child
-
-    id = Lazy(lambda: Seed(1, 1))
-    name = List(["Child A", "Child B", "Child C"])
-
-    # Можно переопределить метод для кастомизации
     @classmethod
-    def create(cls, **kwargs):
-        parent = kwargs.pop('parent', None)
-        child = super().create(**kwargs)
-        if parent:
-            child.parent = parent
-        return child
+    def create(cls, *args, **kwargs):
+        event = kwargs.pop('event', None)
+        instance = super().create(*args, **kwargs)
+        if event:
+            instance.event = event
+            instance.event_id = event.id
+        
+        return instance
+
+
+class EventFactory(SQLAlchemyFactory[models.Event]):
+    __model__ = models.Event
+
+    # Параметры для создания событий
+    name = "Test Event"
+    description = "This is a test event"
+    organizers = []
+
+    @classmethod
+    def create(cls, *args, **kwargs):
+        # Получаем организаторов из kwargs
+        organizers = kwargs.pop('organizers', None)
+        instance = super().create(*args, **kwargs)
+
+        # Если организаторы переданы, устанавливаем их
+        if organizers:
+            instance.organizers = organizers
+
+        return instance
+
 
 # Пример использования
 if __name__ == "__main__":
-    engine = create_engine('sqlite:///:memory:')
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    # Создаем родителя и детей с использованием кастомной фабрики
-    parent = ParentFactory()
-    session.add(parent)
-    session.commit()
-
-    # Создание детей с заданным родителем
-    child1 = ChildFactory(parent=parent)
-    child2 = ChildFactory(parent=parent)
-
-    session.add(child1)
-    session.add(child2)
-    session.commit()
-
-    # Проверка результата
-    print(f"Parent: {parent.name}, Children: {[child.name for child in parent.children]}")
-
-
-
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
-from sqlalchemy.orm import sessionmaker, relationship
-from sqlalchemy.ext.declarative import declarative_base
-from polifactory import Factory, LazyAttribute, Sequence
-
-Base = declarative_base()
-
-# Определим модели
-
-class User(Base):
-    __tablename__ = 'users'
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    posts = relationship('Post', back_populates='user')
-
-class Post(Base):
-    __tablename__ = 'posts'
-
-    id = Column(Integer, primary_key=True)
-    title = Column(String)
-    user_id = Column(Integer, ForeignKey('users.id'))
-    user = relationship('User', back_populates='posts')
-
-# Создаем фабрики
-
-class UserFactory(Factory):
-    class Meta:
-        model = User
+    # Создание организаторов
+    org1 = EmployeeFactory.create(username="Organizer One", roles=["admin"])
+    org2 = EmployeeFactory.create(username="Organizer Two", roles=["admin"])
     
-    id = Sequence(int)
-    name = Sequence(lambda n: f'User {n}')
-
-class PostFactory(Factory):
-    class Meta:
-        model = Post
+    # Создание события с контролируемыми организаторами
+    event = EventFactory.create(organizers=[org1, org2])
     
-    id = Sequence(int)
-    title = Sequence(lambda n: f'Post {n}')
+    # Создание вебинара, связанного с событием
+    webinar = WebinarFactory.create(event=event)
     
-    # Устанавливаем пользователя вручную
-    user = LazyAttribute(lambda obj: UserFactory())
-
-# Пример использования:
-
-# Создаем движок и сессию
-engine = create_engine('sqlite:///:memory:')
-Base.metadata.create_all(engine)
-Session = sessionmaker(bind=engine)
-session = Session()
-
-# Используем фабрики для создания объектов
-user = UserFactory.create()
-posts = PostFactory.create_batch(5, user=user)
-
-# Добавляем пользователя и посты в сессию
-session.add(user)
-session.add_all(posts)
-session.commit()
-
-# Проверяем данные
-for post in session.query(Post).all():
-    print(f"Post ID: {post.id}, Title: '{post.title}', User: '{post.user.name}'")
-
-___________________________________
-
-from polyfactory.factories import Factory, AbstractFactory
-from polyfactory import Field
-
-class PostFactory(Factory):
-    __model__ = Post
-    title = Field(factory=lambda: "Sample Post Title")
-
-class UserFactory(AbstractFactory):
-    __model__ = User
-
-    username = Field(factory=lambda: "user_{0}".format(random.randint(1, 1000)))
-    
-    # Кастомизируем метод
-    def create_with_posts(self, count: int = 3):
-        user = self.build()
-        user.posts = PostFactory.create_batch(count, user=user)
-        return user
-
+    print(event)  # вывод информации о событии
+    print(webinar)  # вывод информации о вебинаре
